e9e26843fde4624e2afa282b06b299c9d9bbb5cf
Cinling <1105412681@qq.com>
2020-08-06 08:56:53 +0800

    添加 TimestampUtil.php 方法

--- src/utils/TimestampUtil.php
+++ src/utils/TimestampUtil.php
@@ -27,4 +27,54 @@ class TimestampUtil {
         $msPart = floatval($tmpArr[1]);
         return floor(($secondPart + $msPart) * 1000);
     }
+
+    /**
+     * 时间戳转日期
+     * @param int $timestamp
+     * @return string
+     */
+    public static function toDate($timestamp) {
+        return date("Y-m-d", $timestamp);
+    }
+
+    /**
+     * 时间戳转日期时间
+     * @param int $timestamp
+     * @return string
+     */
+    public static function toDatetime($timestamp) {
+        return date("Y-m-d H:i:s", $timestamp);
+    }
+
+    /**
+     * 获取今日起始时间戳
+     * @param int $timestamp
+     * @return int
+     */
+    public static function getDateStart($timestamp) {
+        $date = self::toDate($timestamp);
+        return strtotime($date);
+    }
+
+    /**
+     * 获取今日结束时间戳
+     * @param $timestamp
+     * @return int
+     */
+    public static function getDatEnd($timestamp) {
+        $timestamp = self::nextDateTimestamp($timestamp, 1, true);
+        return $timestamp - 1;
+    }
+
+    /**
+     * 获取多少天后的时间戳
+     * @param int $timestamp 时间戳
+     * @param int $days 多少天后
+     * @param bool $roundToDateStart 是否将时间转为今天的0点
+     * @return int
+     */
+    public static function nextDateTimestamp($timestamp, $days = 1, $roundToDateStart = false) {
+        $timestamp += 86400 * $days;
+        return $roundToDateStart ? self::getDateStart($timestamp) : $timestamp;
+    }
 }
