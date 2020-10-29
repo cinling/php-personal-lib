b1d9994ee3ef2729c4fa2e6c3d4a9178a5f640c5
Cinling <1105412681@qq.com>
2020-10-29 10:47:25 +0800

    添加 ValueUtil

---
+++ src/utils/ValueUtil.php
@@ -0,0 +1,41 @@
+<?php
+
+
+namespace cin\personalLib\utils;
+
+
+class ValueUtil {
+
+    /**
+     * @param $value
+     * @return int
+     */
+    public static function toInt($value) {
+        if (!empty($value)) {
+            $value = intval($value);
+        }
+        return $value;
+    }
+
+    /**
+     * @param $value
+     * @return bool
+     */
+    public static function toBool($value) {
+        if ($value === "true" || $value > 0 || $value) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 获取标签字典中的标签
+     * @param mixed[] $dict 标签字典
+     * @param string|int $key 字典key
+     * @param mixed $defaultValue 默认值。当 $key 不存在字典中时返回
+     * @return string
+     */
+    public static function getValue($dict, $key, $defaultValue = "") {
+        return isset($dict[$key]) ? $dict[$key] : $defaultValue;
+    }
+}
